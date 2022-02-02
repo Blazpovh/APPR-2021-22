f6e67e2d4d69eaed1df66cdf1f05370245269423
Blaz Povh <bp1599@student.uni-lj.si>
2022-02-02 23:14:39 +0100

    test

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,7 @@
 # 2. faza: Uvoz podatkov
 
+data_1<-fromJSON(file="podatki/consumptions/consumptions_consumer_2_2021-01-01_2021-12-31.json",method = "R")
+
+consumptions_1<-read.j
 
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
